c6909a57ca966390df4172208fdf8619fb6402ab
Dmytro Vakulenko <33941060+DrSmile444@users.noreply.github.com>
2026-03-02 10:18:09 +0200

    chore: add all.poc.ts to run multiple PoCs together and update package.json scripts

---
+++ src/poc/all.poc.ts
@@ -0,0 +1,15 @@
+import './logs.poc';
+import './moderation.poc';
+import './report.poc';
+
+/**
+ * This file imports all the individual PoCs so they can be run together.
+ * Each PoC is designed to be run independently, but this allows us to execute them all with a single command if desired.
+ *
+ * To run all PoCs together, use the command:
+ * ```
+ * npm run poc:all
+ * ```
+ *
+ * Note that running all PoCs together will execute them sequentially, and each PoC will make its own call to the Gemini API. Be mindful of rate limits and costs when doing this.
+ * */
