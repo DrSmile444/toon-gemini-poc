0dd2095a7bb9869e23f818496aed020ab136bae5
Dmytro Vakulenko <33941060+DrSmile444@users.noreply.github.com>
2026-03-02 15:32:59 +0200

    feat: add token comparison proof of concept to entry points in tsup configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   clean: true,
-  entry: ['src/poc/all.poc.ts', 'src/poc/simple.poc.ts'],
+  entry: ['src/poc/all.poc.ts', 'src/poc/simple.poc.ts', 'src/poc/token-comparison.poc.ts'],
   format: 'esm'
 });
